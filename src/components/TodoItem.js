4e06a54c5e57aa4c74680b65bbb825f5ad7a3fdf
dzenan <dzenankrlic@gmail.com>
2022-04-08 17:46:24 +0200

    handle delete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { List, Box, UnorderedList, Flex, ListItem, Button } from '@chakra-ui/react'
 import { Checkbox } from '@chakra-ui/react'
+import { useDispatch } from 'react-redux'
+import { deleteTodo } from '../redux/todoSlice'
 
 const TodoItem = ({ id, title, completed}) => {
+  const dispatch = useDispatch()
+  const handleDelete = () => {
+    dispatch(deleteTodo({id: id}))
+  }
   return (
     
         
             <ListItem>
                 <Flex padding="3">
                 <Checkbox marginLeft="170px" checked={completed}>{title}</Checkbox>
-                <Button colorScheme='red' size='md'  marginLeft="130" padding="">Delete</Button>
+                <Button colorScheme='red' size='md'  marginLeft="130" padding="" onClick={handleDelete}>Delete</Button>
                 </Flex>
             </ListItem>
         
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -3,24 +3,27 @@ import { createSlice } from "@reduxjs/toolkit";
 const options = {
 name: 'todos',
 initialState: [
-    { id: 1, title: 'todo1', completed: true},
+    { id: 1, title: 'todo1', completed: false},
     { id: 2, title: 'todo2', completed: false},
     { id: 3, title: 'todo3', completed: false},
 ],
 
 reducers: {
-    addTodo(state, action) {
+    addTodo: (state, action) => {
         const newTodo = {
             id: Date.now(),
             title: action.payload.title,
             completed: false
         }
         state.push(newTodo)
+    },
+    deleteTodo: (state, action) => {
+        return state.filter((todo) => todo.id !== action.payload.id)
     }
 }
 }
 
 const todoSlice = createSlice(options)
-export const { addTodo } = todoSlice.actions
+export const { addTodo, deleteTodo } = todoSlice.actions
 
 export default todoSlice.reducer;
